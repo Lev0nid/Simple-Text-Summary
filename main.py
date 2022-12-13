dcf55538bf6c0792635c5bbdba0b60939ec84c64
Егор (Леня) Денисов <66315146+Lev0nid@users.noreply.github.com>
2022-12-14 02:08:35 +0300

    Resolved issues Created a class for model, main is now in a function, edited device choosing line

--- main.py
+++ main.py
@@ -2,25 +2,33 @@ from io import StringIO
 import streamlit as st
 from backend import process
 
-st.title('Simple Text Summary')
 
-st.write('You can get summary from texts either from input area or from uploaded file!')
+def _main():
+    st.title('Simple Text Summary')
 
-form = st.form('input_text_form')
-txt = form.text_area('Input text to get summary')
-uploaded_file = form.file_uploader('Or upload a file', type=['txt'])
-submitted = form.form_submit_button('Submit')
+    st.write('You can get summary from texts either from input area or from uploaded file!')
 
-if submitted:
-    text = ''
-    file_name = 'Summary.txt'
-    if uploaded_file is not None:
-        file_name = uploaded_file.name[:-len('.txt')] + ' summary.txt'
-        text = StringIO(uploaded_file.getvalue().decode("utf-8")).read()
-    else:
-        text = txt
+    form = st.form('input_text_form')
+    txt = form.text_area('Input text to get summary')
+    uploaded_file = form.file_uploader('Or upload a file', type=['txt'])
+    submitted = form.form_submit_button('Submit')
 
-    if text != '':
-        st.download_button('Download summary', process.make_summary(text), file_name)
-    else:
-        st.error('Input is empty')
+    model = process.Summarizer()
+
+    if submitted:
+        text = ''
+        file_name = 'Summary.txt'
+        if uploaded_file is not None:
+            file_name = uploaded_file.name[:-len('.txt')] + ' summary.txt'
+            text = StringIO(uploaded_file.getvalue().decode("utf-8")).read()
+        else:
+            text = txt
+
+        if text != '':
+            st.download_button('Download summary', model.make_summary(text), file_name)
+        else:
+            st.error('Input is empty')
+
+
+if __name__ == '__main__':
+    _main()
